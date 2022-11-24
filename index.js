eafc43d2e573e8dd7a88936b1e6a7be99c67d4f9
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-24 23:41:41 +0600

    addUser-route-updated

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.brpx2ub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+   const token = authHeader.split(' ')[1];
+   jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+    if(err){
+        return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+   })
+}
+
+
 async function run(){
     try {
         const categoriesCollection = client.db("laptopDb").collection("categories");
@@ -36,14 +52,21 @@ async function run(){
             const results = await categoriesCollection.find({}).toArray();
             res.send(results);
         });
+
         app.get('/products', async (req, res) => {
             const {id} = req.query;
             const results = await productsCollection.find({categoryId: id}).toArray();
             res.send(results);
         });
 
-        app.post('/users', async (req, res) => {
+        app.put('/users', async (req, res) => {
             const user = req.body;
+            const email = user.email;
+            const filter =await usersCollection.findOne({email: email});
+            if(filter){
+                const message = "added"
+                return res.send({acknowledged: false, message})
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
